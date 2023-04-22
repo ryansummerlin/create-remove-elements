fec5dd8fea498f897a79c3a710205dc99965f812
ryansummerlin <ryansummerlin777@gmail.com>
2023-04-21 22:04:04 -0700

    implemented javascript to get the add dog button to work

--- create-remove.js
+++ create-remove.js
@@ -8,15 +8,36 @@ add.addEventListener("click", async () => {
         const url = data.message; // URL of new dog image
 
         /*--------------- Get breed (Hint: Parse from URL) ---------------- */
-        // Your code here
+        const urlParts = url.split('/');
+        const breed = urlParts[4];
 
         /*------------ Create new dog card with the url above ------------- */
         /* (use the HTML structure for the current dog image in the index.html
             file to create a new image with the url) */
-        // Your code here
+        // const newDogLi = document.createElement("li");
+        // const newDogFigure = document.createElement("figure");
+        // const newDogImg = document.createElement("img");
+        // const newDogFigcaption = document.createElement("figcaption");
+
+        // newDogImg.setAttribute("src", "url");
+        // newDogFigcaption.innerText = breed;
+
+        // newDogFigure.appendChild(newDogImg);
+        // newDogFigure.appendChild(newDogFigcaption);
+        // newDogLi.appendChild(newDogFigure);
+
+        const newDog = `<li>
+                            <figure>
+                                <img src=${url} />
+                                <figcaption>${breed}</figcaption>
+                            </figure>
+                        </li>`;
+
 
         /* Add the new dog card as a child to the ul in the .gallery element */
-        // Your code here
+        const gallery = document.body.getElementsByClassName("gallery")[0];
+        const galleryUl = gallery.children[0];
+        galleryUl.innerHTML += newDog;
 
     } catch (e) {
         console.log("Couldn't fetch dog :(")
